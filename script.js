3827c234701d524aff16e0deda0dd6373337025c
Markki Meyer <markkimeyer@gmail.com>
2021-01-27 16:41:26 -0600

    img and 5 day

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 var apiKey = "3d5a050005dc5cb9ca5349455c5dc325";
 
+var citySearch = $(".searchInput");
 //current forecast
-var dailyQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=brooklyn&appid=" + apiKey;
+var dailyQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "Hudson" + "&appid=" + apiKey;
 
 
 //extended forecast
 var futureQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=";
+
+
+
+//save input to local storage
+$("button").on("click", function (event) {
+    event.preventDefault();
+    $(citySearch).each(function() {
+    
+    localStorage.setItem("searchedCity", ($(this).val()));
+    
+    })
+
+$(citySearch).each(function() {
+
+    //get input from local storage
+var saveInput = localStorage.getItem("searchedCity");
+$(this).val(saveInput); 
+   $("#search1").html(saveInput);
+    })
+
+
+
+
+
+
 
 $.ajax({
     url: dailyQueryURL,
@@ -20,6 +46,7 @@ $.ajax({
     console.log(lat);
     var lon = (response.coord.lon);
     console.log(lon);
+    console.log(response);
     
 
     var temp = (response.main.temp);
@@ -41,8 +68,20 @@ $.ajax({
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        $(".uv").text("UV Index: " + response.current.uvi)
+        var temp1 = (response.daily[0].temp.day);
+    var tempF1 = (temp1 - 273.15) * 1.80 + 32;
+        $("#temp1").text(tempF1);
+       $("#humidity1").text("Humidity: " + response.daily[0].humidity + "%");
+       var iconcode = (response.daily[0].weather[0].icon);
+       var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+       $("#iconOne").attr('src', iconurl);
 
 
   })
+
+ 
+
+})
 
 })
